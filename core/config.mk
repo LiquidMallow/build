cabea876183c3cb1ecae55e2815801fe22d25c6d
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-05 22:17:59 +0000

    build: Fix global selinux policy inclusion
    We're adding PureNexus-specific policies to an overlay instead of patching
    external/sepolicy, so we need to make sure it's included to prevent
    misbehaving systems
    
    Change-Id: I6fe702fcc1fde7ce2570d916540f622232fb9c88

--- core/config.mk
+++ core/config.mk
@@ -871,4 +871,6 @@ endif
 RSCOMPAT_32BIT_ONLY_API_LEVELS := 8 9 10 11 12 13 14 15 16 17 18 19 20
 RSCOMPAT_NO_USAGEIO_API_LEVELS := 8 9 10 11 12 13
 
+include vendor/nougat/sepolicy/sepolicy.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk
