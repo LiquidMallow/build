c830fb6ba06de2b527350b3fb41e14bb05bd24eb
Corey Edwards <ensabahnur16@gmail.com>
2015-10-12 20:43:37 -0500

    Flash SuperSU when not block-based
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Don't do that!!
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -665,9 +665,11 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
                    ""+input_zip.read("SYSTEM/addon.d/UPDATE-SuperSU.zip"))
     script.Mount("/system")
     script.FlashSuperSU()
-
-  if block_based:
-    script.Unmount("/system")
+  else:
+    script.Print("Flashing SuperSU...")
+    common.ZipWriteStr(output_zip, "supersu/supersu.zip",
+                   ""+input_zip.read("SYSTEM/addon.d/UPDATE-SuperSU.zip"))
+    script.FlashSuperSU()
 
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
